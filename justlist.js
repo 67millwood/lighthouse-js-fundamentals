f51f2f66422ed57d355ef8d6afb080b4d28ad6e2
67millwood <info@track2development.com>
2018-09-13 01:02:53 +0000

    loopy lighthouse

---
+++ justlist.js
@@ -0,0 +1,9 @@
+
+
+function counter(end) {
+  for (i = 0; i <= end; i++) {
+    console.log(i);
+  }
+}
+
+console.log(counter(15));
---
+++ loopy1.js
@@ -0,0 +1,22 @@
+// loopy numbers 100 to 200
+
+
+function counter(start, end) {
+  for (var i = 0; i + start <= end; i++) {
+    if ((i + start) % 3 === 0 && (i + start) % 4 === 0) {
+      console.log("LoopyLighthouse");
+    }
+    else if ((i + start) % 3 === 0) {
+      console.log("Loopy");
+    }
+    else if ((i + start) % 4 === 0) {
+      console.log("Lighthouse");
+    }
+    else {
+      console.log(i + start);
+    }
+  }
+}
+
+counter(100, 200);
+
