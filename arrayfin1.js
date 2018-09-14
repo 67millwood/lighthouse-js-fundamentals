4741ef7974952a4dff55679c18938abd5c817cbd
67millwood <info@track2development.com>
2018-09-14 19:22:22 +0000

    loop js exercise

---
+++ arrayfin1.js
@@ -0,0 +1,20 @@
+
+var repeatNumbers = function first(num) {
+
+  var array = [];
+  var bigarray = [];
+
+  for (a = 0; a < num.length; a++) {
+    for (i = 0; i < num[a][1]; i++) {
+      array.push(num[a][0]);
+  }
+      var entry = array.join("").toString() + ", ";
+      bigarray.push(entry);
+      array = [];
+  }
+      return bigarray.join("").toString();
+}
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
---
+++ tesjs.js
@@ -0,0 +1,8 @@
+
+var array = [];
+
+function first(num) {
+    return (num.length);
+  }
+
+console.log(first([[4, 6], [5, 10]]));
