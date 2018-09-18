d123bf61d984726638c7df5088f4bc0e2a8af401
67millwood <info@track2development.com>
2018-09-18 18:43:56 +0000

    kata 6 finally done properly

--- kata6.js
+++ kata6.js
@@ -1,30 +1,43 @@
 
-
 var multiplicationTable = function(maxValue) {
 
-  var firstarray = [];
-  var secondarray = [];
-      for (a = 1; a <= maxValue; a++) {
-        for (i = 1; i <=maxValue; i++) {
-          firstarray.push(i * a);
-      }
-        secondarray.push(firstarray);
-        firstarray = [];
-    }
+  // create the arrays that create the table
+  var array1 = [];
+  var array2 = [];
+  var array3 = [];
+  var dash = "-"
+  var space = " ";
 
-
-
- //   convert array to text
-    for (b = 0; b < secondarray.length; b++) {
-      var x = secondarray[b].toString();
-      var border = "| ";
-      x = x.split(',').join(border);
-      var y = (y + "\n" + x);
-    }
-      return (y);
+for (a = 1; a <= maxValue; a++) {
+  for (i = 1; i <= maxValue; i++) {
+    array1.push(a * i);
+  }
+  array2.push(array1);
+  array1 = [];
 
 }
 
-console.log(multiplicationTable(5));
-// multiplicationTable(12);
+  // create the borders between the rows
+  var bignum = array2[maxValue - 1][maxValue - 1];
+  var big1 = (bignum.toString().length + 2);
 
+  //  create the spacing and borders between the colums
+  for (a = 0; a < maxValue; a++) {
+    for (i = 0; i < maxValue; i++) {
+       array1.push("+" + dash.repeat(big1));
+       var spacer = (big1 - 1 - (array2[a][i].toString().length));
+       array3.push("| " + array2[a][i] + space.repeat(spacer));
+  }
+  var borders = (array1.join("") + "+");
+  console.log(borders);
+  console.log(array3.join("") + "|");
+  array1 = [];
+  array3 = [];
+
+} console.log(borders);
+
+
+
+}
+
+multiplicationTable(12);
